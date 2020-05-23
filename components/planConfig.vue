057e13fbbc4bde6e6c57ebcd66bbe7c797549f74
Yusei Ito <yuseikun@Outlook.jp>
2020-05-23 23:12:55 +0900

    旅行計画の設定をいれるユーティリティ

---
+++ components/planConfig.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="mx-auto" max-width="600" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4"></div>
+        <v-list-item-title class="headline mb-1">
+          <v-text-field label="旅行のタイトル"></v-text-field>
+        </v-list-item-title>
+        <v-row justify="space-around">
+          <v-date-picker
+            v-model="selected"
+            color="#CC3F50"
+            range
+          ></v-date-picker>
+        </v-row>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selected: []
+    }
+  }
+}
+</script>
---
+++ pages/plan.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout>
+    <v-container>
+      <plan-config />
+    </v-container>
+  </v-layout>
+</template>
+<script>
+import planConfig from '@/components/planConfig'
+export default {
+  components: {
+    planConfig
+  },
+  mounted() {
+    this.$nuxt.$emit('updateHeader', '旅行を計画する')
+  }
+}
+</script>
