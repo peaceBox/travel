aa6796fdb1ca345ca4a9feab38b75a6086964395
Yusei Ito <yuseikun@Outlook.jp>
2020-05-23 16:43:48 +0900

    Modify : center

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <v-layout row justify-center align-center>
     <GmapMap
-      :center="{ lat: 10, lng: 10 }"
-      :zoom="7"
+      :center="{ lat: 34.402934, lng: 132.456019 }"
+      :zoom="10"
       map-type-id="terrain"
       style="width: 100vw; height:100vh;margin:0;"
     >
