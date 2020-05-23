e0f7d5a83b6b6d66b4ea90b94ccaf830e4d32d51
Yusei Ito <yuseikun@Outlook.jp>
2020-05-24 00:36:55 +0900

    Refactoring

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
+// This API key must have access to Maps JavaScript API
+const APIKey = 'AIzaSyCgOm__vdrF5Ta3wWvrVW6Wq7FSMdU7O-k'
+
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCgOm__vdrF5Ta3wWvrVW6Wq7FSMdU7O-k',
+    key: APIKey,
     libraries: 'places'
   }
 })
