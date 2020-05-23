fc9fc99ec485c7d7d3d3fb3a07f774eb86aedc06
Yusei Ito <yuseikun@Outlook.jp>
2020-05-24 01:02:54 +0900

    UIの改善

--- components/placeDetailsModal.vue
+++ components/placeDetailsModal.vue
@@ -2,7 +2,9 @@
   <v-dialog :value="value" max-width="600" @input="$emit('input', value)">
     <v-card>
       <v-card-title class="headline">
-        {{ dialogContent !== null ? dialogContent.name : '' }}
+        <span style="color:#CC3F50;font-weight:bold;">{{
+          dialogContent !== null ? dialogContent.name : ''
+        }}</span>
       </v-card-title>
 
       <img :src="data.icon" />
@@ -13,13 +15,9 @@
         <v-btn color="gray darken-1" text @click="$emit('input', false)"
           >閉じる</v-btn
         >
-        <v-btn
-          color="green darken-1"
-          text
-          street-address
-          @click="$emit('input', false)"
-          >予定に追加</v-btn
-        >
+        <v-btn color="#CC3F50" street-address @click="$emit('input', false)">
+          <span style="color:#fff;font-weight:bold;">予定に追加</span>
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
