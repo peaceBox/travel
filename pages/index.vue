3541d6f42e674d407347e9f5029532fc02609c4d
Yusei Ito <yuseikun@Outlook.jp>
2020-05-23 23:13:14 +0900

    ピン打ち実装完了

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,16 @@
       <GmapMarker
         v-for="(m, index) in markers"
         :key="index"
-        :position="m.position"
+        :position="{ lat: m.lat, lng: m.lng }"
         :clickable="true"
-        :draggable="true"
-        @click="center = m.position"
+        @click="onPinClick(m)"
       />
     </GmapMap>
   </v-layout>
 </template>
 
 <script>
+import data from '@/plugins/data.json'
 export default {
   data() {
     return {
@@ -30,6 +30,42 @@ export default {
   },
   mounted() {
     this.$nuxt.$emit('updateHeader', '見る')
+    this.createTable()
+  },
+  methods: {
+    createTable() {
+      switch (decodeURI(this.$route.query.type)) {
+        case '見る/全部':
+          this.markers = data.filter((e) => {
+            const pattern = /見る\/.*/
+            return e.genre.match(pattern)
+          })
+          break
+        case '食べる':
+          // TODO: レストラン群
+          break
+        case 'ホテル':
+          // ホテルに。
+          break
+        case '遊ぶ/全部':
+          this.markers = data.filter((e) => {
+            const pattern = /遊ぶ\/.*/
+            return e.genre.match(pattern)
+          })
+          break
+        case '全部':
+          this.markers = data
+          break
+        default:
+          console.log('Default called')
+          this.markers = data.filter((e) => {
+            return e.genre === decodeURI(this.$route.query.type)
+          })
+      }
+    },
+    onPinClick(object) {
+      window.alert(object.name)
+    }
   }
 }
 </script>
